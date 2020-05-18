887f0ade4d5cf07686a1bc58ac6f6b3948ce15df
hyungkyu <hyungkyu@deepeyes.co.uk>
2020-05-18 17:37:09 +0100

    init commit

---
+++ BankAccount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BankAccount
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("=======================");
+      
+      var ba = new BankAccount(100);
+      ba.Deposit(50);
+      Console.WriteLine(ba.Balance);
+    }
+    
+    public class BankAccount
+    {
+      public int Balance { get; private set; }
+
+      public BankAccount(int balance)
+      {
+        Balance = balance;
+      }
+
+      public void Deposit(int amount)
+      {
+        Balance += amount;
+      }
+    }
+    
+    [TestFixture]
+    public class BankAccountTests
+    {
+      [Test]
+      public void DepositTest()
+      {
+        var ba = new BankAccount(0);
+        ba.Deposit(100);
+        Assert.That(ba.Balance, Is.EqualTo(100));
+      }
+    }
+    
+  }
+}
